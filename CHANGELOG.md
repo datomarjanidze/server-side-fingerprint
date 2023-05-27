33b49c37bafc589b33a8300cfffbccb6bbfcd47f
dmarjapplmacb1 <dmarjapplmacb1@gmail.com>
2023-05-27 15:34:03 +0400

    refactor the library

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 1.1.0 (2023-05-27)
+
+Refactor the library.
+
 # 1.0.5 (2023-05-26)
 
 Remove `networkInterfaces` data from the fingerprint, it's unstable.
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {
   EOL,
   availableParallelism,
   arch,
+  type CpuInfo,
   cpus,
   devNull,
   endianness,
@@ -14,105 +15,58 @@ import {
   tmpdir,
   totalmem,
   type,
-  userInfo,
   type UserInfo,
+  userInfo,
   version
 } from 'node:os'
 
-class ServerSideFingerprint {
-  generateFingerprintData(): string {
-    return JSON.stringify({
-      EOL: this.getEOL(),
-      availableParallelism: this.getAvailableParallelism(),
-      arch: this.getArch(),
-      cpuInfo: this.getCpuInfo(),
-      devNull: this.getDevNull(),
-      endianness: this.getEndianness(),
-      homedir: this.getHomedir(),
-      hostname: this.getHostname(),
-      machine: this.getMachine(),
-      platform: this.getPlatform(),
-      release: this.getRelease(),
-      tmpdir: this.getTmpdir(),
-      totalmem: this.getTotalmem(),
-      type: this.getType(),
-      userInfo: this.getUserInfo(),
-      version: this.getVersion()
-    })
-  }
+export interface FingerprintData {
+  EOL: string
+  availableParallelism: number
+  arch: string
+  cpuInfo: CpuPartialInfo[]
+  devNull: string
+  endianness: string
+  homedir: string
+  hostname: string
+  machine: string
+  platform: string
+  release: string
+  tmpdir: string
+  totalmem: number
+  type: string
+  userInfo: UserInfo<string>
+  version: string
+}
 
-  private getEOL(): string {
-    return EOL
-  }
+export type CpuPartialInfo = Pick<CpuInfo, 'model' | 'speed'>
 
-  private getAvailableParallelism(): number {
-    return availableParallelism()
-  }
-
-  private getArch(): string {
-    return arch()
-  }
-
-  private getCpuInfo(): { model: string; speed: number }[] {
-    return cpus().reduce((acc, cpu) => {
+export function generateFingerprintData(): FingerprintData {
+  return {
+    EOL,
+    availableParallelism: availableParallelism(),
+    arch: arch(),
+    cpuInfo: cpus().reduce((acc: CpuPartialInfo[], cpu) => {
       acc.push({ model: cpu.model, speed: cpu.speed })
       return acc
-    }, [])
-  }
-
-  private getDevNull(): string {
-    return devNull
-  }
-
-  private getEndianness(): string {
-    return endianness()
-  }
-
-  private getHomedir(): string {
-    return homedir()
-  }
-
-  private getHostname(): string {
-    return hostname()
-  }
-
-  private getMachine(): string {
-    return machine()
-  }
-
-  private getPlatform(): string {
-    return platform()
-  }
-
-  private getRelease(): string {
-    return release()
-  }
-
-  private getTmpdir(): string {
-    return tmpdir()
-  }
-
-  private getTotalmem(): number {
-    return totalmem()
-  }
-
-  private getType(): string {
-    return type()
-  }
-
-  private getUserInfo(): UserInfo<string> {
-    return userInfo()
-  }
-
-  private getVersion(): string {
-    return version()
+    }, []),
+    devNull,
+    endianness: endianness(),
+    homedir: homedir(),
+    hostname: hostname(),
+    machine: machine(),
+    platform: platform(),
+    release: release(),
+    tmpdir: tmpdir(),
+    totalmem: totalmem(),
+    type: type(),
+    userInfo: userInfo(),
+    version: version()
   }
 }
 
 export function generateFingerprint(): string {
-  const serverSideFingerprint = new ServerSideFingerprint()
-
   return createHash('md5')
-    .update(serverSideFingerprint.generateFingerprintData())
+    .update(JSON.stringify(generateFingerprintData()))
     .digest('hex')
 }
